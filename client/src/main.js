020b05447bb1febf9b219baeed517c59d4a7f77c
Sophie04 <breharu_alexandra@yahoo.com>
2021-03-01 15:42:13 +0200

    register working+styling

--- client/src/main.js
+++ client/src/main.js
@@ -3,11 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+
 /* eslint-disable no-new */
 new Vue({
+  vuetify: new Vuetify(),
   el: '#app',
   router,
   components: { App },
