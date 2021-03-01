7cd990fdbba455042b948cae4d692823e143df99
Sophie04 <breharu_alexandra@yahoo.com>
2021-03-01 15:49:09 +0200

    small change

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -33,12 +33,6 @@ export default {
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email, 
-        password: this.password
-      })
-      console.log(response.data)
-
       try {
         await AuthenticationService.register({
           email: this.email,
