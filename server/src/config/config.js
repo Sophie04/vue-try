476b33660f0e39a189732b182e4e94e43908373e
Sophie04 <breharu_alexandra@yahoo.com>
2021-03-01 19:00:27 +0200

    register and login with hashed password working

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -9,5 +9,8 @@ module.exports = {
         host: process.env.HOST || 'localhost',
         storage: './vue-try.sqlite'
       }
+    },
+    authentication: {
+      jwtSecret: process.env.JWT_SECRET || 'secret'
     }
 }
